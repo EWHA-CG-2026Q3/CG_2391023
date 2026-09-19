a2c67e87ed471f72cc7a2849ae8c89685bd57980
ek0821 <ek0821@ewha.ac.kr>
2026-09-20 00:39:23 +0900

    S04-2 다이아몬드 삼각형 8개로 구현

--- Assets/S04/DiamondMesh.cs
+++ Assets/S04/DiamondMesh.cs
@@ -17,5 +17,30 @@ public class DiamondMesh : MonoBehaviour
             new Vector3(0.5f, 1f, 0.5f),  // 4
             new Vector3(0.5f, -1f, 0.5f)  // 5
         };
+        // 다이아몬드의 삼각형 8개
+        int[] triangles = new int[]
+        {
+            // 위쪽 4면
+            4, 1, 0,
+            4, 2, 1,
+            4, 3, 2,
+            4, 0, 3,
+
+            // 아래쪽 4면
+            5, 0, 1,
+            5, 1, 2,
+            5, 2, 3,
+            5, 3, 0
+        };
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        GetComponent<MeshRenderer>().sharedMaterial =
+            new Material(Shader.Find("Universal Render Pipeline/Lit"));
     }
 }
